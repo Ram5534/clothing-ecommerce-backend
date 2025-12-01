9f68f920684d8fd4ec45a575fd6abf5c87b4d23f
Ram5534 <kurakularammohanrao@gmail.com>
2025-12-01 12:16:45 +0530

    first

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { requireAuth } = require("../middleware/authMiddleware");
 const { createOrder, getOrderById } = require("../controllers/orderController");
 
-router.post("/add-orders",  createOrder);
+router.post("/",  createOrder);
 router.get("/:id",  getOrderById);
 
 module.exports = router;
