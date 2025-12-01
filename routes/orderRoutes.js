efa83e483de21763ac3560b119e91af0b27008e7
Ram5534 <kurakularammohanrao@gmail.com>
2025-12-01 11:41:47 +0530

    first commit

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { requireAuth } = require("../middleware/authMiddleware");
 const { createOrder, getOrderById } = require("../controllers/orderController");
 
-router.post("/",  createOrder);
-router.get("/:id",  getOrderById);
+router.post("/",requireAuth,  createOrder);
+router.get("/:id",requireAuth,  getOrderById);
 
 module.exports = router;
